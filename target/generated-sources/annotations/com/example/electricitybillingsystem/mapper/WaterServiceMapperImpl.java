ba5265e166c2994117009e21e0bcca6f736b478a
Nampti <nguyenhoainama3k55@gmail.com>
2024-04-21 20:19:33 +0700

    Update report

--- target/generated-sources/annotations/com/example/electricitybillingsystem/mapper/WaterServiceMapperImpl.java
+++ target/generated-sources/annotations/com/example/electricitybillingsystem/mapper/WaterServiceMapperImpl.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2024-04-17T20:33:47+0700",
+    date = "2024-04-21T20:19:12+0700",
     comments = "version: 1.5.5.Final, compiler: Eclipse JDT (IDE) 3.38.0.v20240325-1403, environment: Java 17.0.10 (Eclipse Adoptium)"
 )
 @Component
