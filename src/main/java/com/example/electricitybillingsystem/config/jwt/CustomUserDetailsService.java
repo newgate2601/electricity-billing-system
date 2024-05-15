e17e3a3891026e4571a60874ea8888595cc0aa08
PhucPT09 <pphuc9122002@gmail.com>
2024-05-15 14:23:01 +0700

    push

--- src/main/java/com/example/electricitybillingsystem/config/jwt/CustomUserDetailsService.java
+++ src/main/java/com/example/electricitybillingsystem/config/jwt/CustomUserDetailsService.java
@@ -1,25 +1,25 @@
-package com.example.electricitybillingsystem.config.jwt;
-
-import com.example.electricitybillingsystem.model.User;
-import com.example.electricitybillingsystem.repository.UserRepository;
-import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.stereotype.Component;
-
-import java.util.Optional;
-
-@Component
-@RequiredArgsConstructor
-public class CustomUserDetailsService implements UserDetailsService {
-    private final UserRepository userRepository;
-
-    @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        Optional<User> userOptional = userRepository.findByUsername(username);
-        return userOptional
-                .map(CustomUserDetails::new)
-                .orElseThrow(() -> new UsernameNotFoundException("user not found with username :" + username));
-    }
-}
+//package com.example.electricitybillingsystem.config.jwt;
+//
+//import com.example.electricitybillingsystem.model.User;
+//import com.example.electricitybillingsystem.repository.UserRepository;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.security.core.userdetails.UserDetails;
+//import org.springframework.security.core.userdetails.UserDetailsService;
+//import org.springframework.security.core.userdetails.UsernameNotFoundException;
+//import org.springframework.stereotype.Component;
+//
+//import java.util.Optional;
+//
+//@Component
+//@RequiredArgsConstructor
+//public class CustomUserDetailsService implements UserDetailsService {
+//    private final UserRepository userRepository;
+//
+//    @Override
+//    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+//        Optional<User> userOptional = userRepository.findByUsername(username);
+//        return userOptional
+//                .map(CustomUserDetails::new)
+//                .orElseThrow(() -> new UsernameNotFoundException("user not found with username :" + username));
+//    }
+//}
